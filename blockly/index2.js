c039d85df41bfb3bacc429115e4f4d311846096f
Wesley Chalmers <wesleyjchalmers@gmail.com>
2017-07-04 20:18:27 +0000

    Add extended toolbox

--- blockly/index2.js
+++ blockly/index2.js
@@ -1,5 +1,25 @@
-var workspace = Blockly.inject('blocklyDiv',
+var blocklyArea = document.getElementById("blocklyArea")
+var blocklyDiv = document.getElementById("blocklyDiv")
+var workspace = Blockly.inject(blocklyDiv,
 			       {toolbox: document.getElementById('toolbox')});
+var onresize = function(e) {
+    var element = blocklyArea
+    var x = 0
+    var y = 0
+    do {
+	x += element.offsetLeft
+	y += element.offsetTop
+	element = element.offsetParent
+    } while (element);
+    blocklyDiv.style.left = x + "px"
+    blocklyDiv.style.top = y + "px"
+    blocklyDiv.style.width = blocklyArea.offsetWidth + "px"
+    blocklyDiv.style.height = blocklyArea.offsetHeight + "px"
+};
+window.addEventListener("resize", onresize, false)
+onresize()
+Blockly.svgResize(workspace);
+
 
 function upload(){
     var filename = "cache.py"
