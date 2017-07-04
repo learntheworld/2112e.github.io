2e134f7fd1d3a699f130aa3a50cb78c4d827d552
Wesley Chalmers <wesleyjchalmers@gmail.com>
2017-07-04 00:55:29 +0000

    Convert str to int when calculating sleep-until time

--- blockly/settings.js
+++ blockly/settings.js
@@ -1,6 +1,6 @@
 function upload(){
     var filename = "config.json"
-    var text = JSON.stringify({"sleep-after":document.getElementById("sleep-after").value})
+    var text = JSON.stringify({"sleep-after":Number(document.getElementById("sleep-after").value)})
     var data = "\x01"+filename+"\x02"+text+"\x03"
     var ws = new WebSocket("ws://192.168.4.1:2112")
     ws.onopen = function(){
